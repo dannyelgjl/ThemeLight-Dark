9c6d1a173e5d3718434a70ca26c37c56c084b810
dannyelgjl <dannyelgjl@gmail.com>
2020-03-30 13:24:19 -0300

    tema light ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { ThemeProvider } from "styled-components";
+
+import light from "./styles/themes/light";
+import dark from "./styles/themes/light";
 
 import GlobalStyle from "./styles/global";
 import Header from "./components/Header";
 
 function App() {
+  const [theme, setTheme] = useState(light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === "light" ? dark : light);
+  };
+
   return (
-    <div className="App">
-      <GlobalStyle />
-      <Header />
-    </div>
+    <ThemeProvider theme={light}>
+      <div className="App">
+        <GlobalStyle />
+        <Header />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import Switch from "react-switch";
+import { ThemeContext } from "styled-components";
+import { shade } from "polished";
 
 import { Container } from "./styles";
 
-const Header: React.FC = () => {
-  return <Container>ok</Container>;
+interface Props {
+  toggleTheme(): void;
+}
+
+const Header: React.FC<Props> = () => {
+  const { colors } = useContext(ThemeContext);
+
+  return (
+    <Container>
+      ok
+      <Switch
+        onChange={toggleTheme}
+        checked={false}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        height={10}
+        width={40}
+        handleDiameter={20}
+        offColor={shade(0.15, colors.primary)}
+        onColor={colors.secundary}
+      />
+    </Container>
+  );
 };
 
 export default Header;
